4fa52f23aa96361c006a3f8bbf98133a05777e0d
Alberto Armijo <armijoalb@gmail.com>
2018-03-23 12:37:24 +0100

    añadido slide up panel

--- PathFinder/app/src/main/java/com/tripmaker/alberto/pathfinder/MapsActivity.java
+++ PathFinder/app/src/main/java/com/tripmaker/alberto/pathfinder/MapsActivity.java
@@ -8,7 +8,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
